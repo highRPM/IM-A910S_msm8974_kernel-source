5e6e8bd0ea6f6d88f773e03ff367d861bf6287ef
HighRPM <ohsunghwan5@naver.com>
2014-12-27 00:47:35 +0900

    overclock 2.45GHZ

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -454,7 +454,7 @@ static void get_krait_bin_format_b(struct platform_device *pdev,
 	struct resource *res;
 	void __iomem *base;
 
-	*speed = 0;
+	*speed = 3;
 	*pvs = 0;
 	*pvs_ver = 0;
 
@@ -506,7 +506,11 @@ static void get_krait_bin_format_b(struct platform_device *pdev,
 	}
 
 	dev_info(&pdev->dev, "PVS version: %d\n", *pvs_ver);
-
+#ifdef CONFIG_CPU_OC
+	dev_info(&pdev->dev, "DooMLoRD: Forcing CPU OC!\n");
+	*speed = 3;
+	dev_info(&pdev->dev, "CPU OC: Speed bin: %d\n", *speed);
+#endif
 	devm_iounmap(&pdev->dev, base);
 }
 
